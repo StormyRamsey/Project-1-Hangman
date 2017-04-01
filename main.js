18c12d181586c05aca4c09e326895426f40960e1
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-01 12:24:16 -0400

    added js variables

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+
+var canvas = document.getElementById('#stage'),
+    word = document.getElementById('#word'),
+    letters = document.getElementById('#letters'),
+    wordToGuess,
+    wordLength,
+    badGuesses,
+    correctGuesses;
+});
+
+function initialize() {
+  var helptext = $('helptext')
+
+}
