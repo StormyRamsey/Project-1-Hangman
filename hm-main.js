cc813010ea9a8ca1fb4e090d6fae4c2fa2b38e02
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-01 13:55:08 -0400

    made additions to js taking scratching and taking a different approach.

--- hm-main.js
+++ hm-main.js
@@ -5,26 +5,26 @@ var canvas = document.getElementById('stage');
 var word = document.getElementById('word');
 var letters = document.getElementById('letters');
 
-var wordToGuess;
+var wordToGuess ;
 var wordLength;
 var badGuesses;
 var correctGuesses;
 
 // initialize game area -------
 function initializeGame() {
-  var helptext = $('helptext');
-  $('#play').css('display', 'inline-block').click(playGame);
+var playGame = document.getElementById(#play).onclick(playGame)
+  /*$('#play').css('display', 'inline-block').click(playGame);
   $('#clear').css('display', 'inline-block').click(resetGame);
-}
+*/
 
-function playGame() {
-  alert('Game started');
-}
+  function playGame() {
+    alert('Game started');
+  }
 
-function resetGame() {
-  alert('Game has been reset');
+  function resetGame() {
+    alert('Game has been reset');
+  }
 }
-
 //Canvas area ---------
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
