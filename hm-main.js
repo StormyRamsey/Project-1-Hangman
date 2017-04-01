64ddd1fcd63a1fa4b954256d82f813c169881d29
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-01 13:18:27 -0400

    added canvas

---
+++ hm-main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+
+var canvas = document.getElementById('stage');
+var word = document.getElementById('word');
+var letters = document.getElementById('letters');
+
+var wordToGuess;
+var wordLength;
+var badGuesses;
+var correctGuesses;
+
+// initialize game area -------
+function initializeGame() {
+  var helptext = $('helptext');
+  $('#play').css('display', 'inline-block').click(playGame);
+  $('#clear').css('display', 'inline-block').click(resetGame);
+}
+
+function playGame() {
+  alert('Game started');
+}
+
+function resetGame() {
+  alert('Game has been reset');
+}
+
+//Canvas area ---------
+  var canvas = document.getElementById('myCanvas');
+  var context = canvas.getContext('2d');
+
+      context.font = '40pt Calibri';
+      context.fillStyle = 'blue';
+      context.fillText('Hello World!', 150, 100);
+
+
+});
