8be2967a6d6bfee8507f3a481150d6be96479778
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-01 20:47:19 -0400

    simplifed on-click function

--- hm-main.js
+++ hm-main.js
@@ -1,15 +1,17 @@
-var wordBank = ['apple', 'orange', 'peanut'];
+
+let numOfMisses = 0;
+const maxMisses =7;
+let currentWord = null;
+let guesses = [];
+var wordBank = ['apple', 'orange', 'peanut', 'jelly', 'oatmeal', 'rice', 'kale'];
 
 
-
-// start button is working; needs to show random number and underscore
+// start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
+          currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
+          console.log('hello world!');
 
-        var word = 'abcd123456';
-        var wordLength = word.length;
-        var underscores = '';
-    for(i=0; i<wordLength; i++) {
-        underscores = underscores + '_'
-      }
-document.getElementById('random-words').innerHTML = wordBank[0];
-};
+      displayLetter();
+}
+/* At this point when I click the start button I can see the first word
+ in the array's index an the underscores don't have space betwen each letter. */
