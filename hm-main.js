4a965861e01c8ee17ae99e314797081840257c5a
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-01 22:51:09 -0400

    added random word to guess, displays as underscores and length of word.

--- hm-main.js
+++ hm-main.js
@@ -2,16 +2,44 @@
 let numOfMisses = 0;
 const maxMisses =7;
 let currentWord = null;
-let guesses = [];
-var wordBank = ['apple', 'orange', 'peanut', 'jelly', 'oatmeal', 'rice', 'kale'];
-
+var guess = [];
+var wordBank = ['apple', 'oatmeal', 'rice', 'kale', 'spinach', 'carrots'];
+var word = document.getElementById('blank-word')
 
 // start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
-          currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
-          console.log('hello world!');
+          var placeholder = '',
+          abc = ['A','B','C','D','E','F','G','H','I','J','K','L',
+                'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+          numOfMisses = 0;
+          wordToGuess = getWord();
+          wordLength = wordToGuess.length;
 
-      displayLetter();
+          for (var i = 0; i < wordLength; i++) {
+            placeholder += '_';
+            }
+          word.innerHTML = placeholder;
 }
-/* At this point when I click the start button I can see the first word
- in the array's index an the underscores don't have space betwen each letter. */
+function getWord() {
+
+  return currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
+}
+//currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
+
+//-----------------------------------------------------
+//function displayLetter() {}
+  /*var placeholder = '',
+    for (var i=0; i < wordBank; i++) {
+       placeholder += '_';
+    }
+word.innerHTML = placeholder; */
+
+
+//document.getElementById('blank-word').innerHTML = displayLetter();
+
+
+//----------------------------------------------------
+//function guessLetter() {}
+
+//----------------------------------------------------
+//function usersGuess() {}
