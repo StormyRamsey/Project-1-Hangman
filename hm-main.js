56c259742dde90a37a3d3690bda64185c0e090f4
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-03 15:46:30 -0400

    input button is returning value through console.log

--- hm-main.js
+++ hm-main.js
@@ -3,10 +3,11 @@ let numOfMisses = 0;
 const maxMisses =7;
 let currentWord = null;
 var guess = [];
-var wordBank = ['apple', 'oatmeal', 'rice', 'kale', 'spinach', 'carrots'];
-var word = document.getElementById('blank-word')
+var wordToGuess;
+var wordBank = ['apple', 'oatmeal', 'rice', 'kale', ];
+var word = document.getElementById('blank-word');
 var letters = document.getElementById('start-button');
-
+var letterInput = document.getElementById('letter-input');
 // start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
 
@@ -25,12 +26,12 @@ document.getElementById('new-game').onclick = function startGame() {
       function getWord(word) {
         return currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
 }//----------------------------------------------------
- document.getElementById('submit-letter').onclick = function displayBlankWord(){
+ document.getElementById('submit-letter').onclick = function displayBlankWord(word,letter){
    var display ='';
     for(var i=0; i < wordBank.length; i++){
       display = display + '_';
     }
-    return display;
+    console.log(letterInput.value)
 
   function isLetterInWord(word,letter){
       if(word.search(letter) != -1){
