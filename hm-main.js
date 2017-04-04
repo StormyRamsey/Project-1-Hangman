09dd1e81bcf4eb7541dc2a14c917bbdf5000f165
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-03 21:01:56 -0400

    console.log value added again

--- hm-main.js
+++ hm-main.js
@@ -2,12 +2,12 @@
 let numOfMisses = 0;
 const maxMisses =7;
 let currentWord = null;
-var guess = [];
+//var guess = [];
 var wordToGuess;
 var wordBank = ['apple', 'oatmeal', 'rice', 'kale', ];
 var word = document.getElementById('blank-word');
 var letters = document.getElementById('start-button');
-var letterInput = document.getElementById('letter-input');
+var letterInput = document.getElementById('letter');
 // start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
 
@@ -26,22 +26,28 @@ document.getElementById('new-game').onclick = function startGame() {
       function getWord(word) {
         return currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
 }//----------------------------------------------------
- document.getElementById('submit-letter').onclick = function displayBlankWord(word,letter){
-   var display ='';
-    for(var i=0; i < wordBank.length; i++){
-      display = display + '_';
-    }
-    console.log(letterInput.value)
+document.getElementById('submit-letter').onclick = function displayBlankWord(word,letter){
+ var display ='';
+  for(var i=0; i < wordBank.length; i++){
+    display = display + '_';
+  }
+  console.log(letterInput.value);
+}
+ /*------------------------------------------------------
+   function guessOne() {
+     var guess = document.getElementById('letterInput');
+     var showThisMessage = '';
 
-  function isLetterInWord(word,letter){
-      if(word.search(letter) != -1){
+       if(wordBank.length !== 1){
+         showThisMessage = 'Please enter one letter.'
+       } else {
+           var i = 0;
+             for (i = 0; i < wordBank.length; i++){
+               if (word[i] === guess);
+         }
 
-      } else {
-        console.log(false);
-      }
+       }
 
-    }
- }
-
+     }*/
 //----------------------------------------------------
 //function usersGuess() {}
