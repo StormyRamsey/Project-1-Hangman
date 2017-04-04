34a5ec1247743c1eb4b8e0fa9efb535e72b88b9c
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-03 23:51:35 -0400

    added console.log(Object.values(wordBank)), but this is showing blank spaces for each array in the index; need to target the characters in each word.

--- hm-main.js
+++ hm-main.js
@@ -2,12 +2,12 @@
 let numOfMisses = 0;
 const maxMisses =7;
 let currentWord = null;
-//var guess = [];
-var wordToGuess;
+var wordToGuess = '';
 var wordBank = ['apple', 'oatmeal', 'rice', 'kale', ];
 var word = document.getElementById('blank-word');
 var letters = document.getElementById('start-button');
 var letterInput = document.getElementById('letter');
+
 // start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
 
@@ -26,26 +26,21 @@ document.getElementById('new-game').onclick = function startGame() {
       function getWord(word) {
         return currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
 }//----------------------------------------------------
-document.getElementById('submit-letter').onclick = function displayBlankWord(){
- var display ='';
+document.getElementById('submit').onclick = function displayBlankWord(){
+  var answerArray = [];
+    for (var i = 0; i < wordBank.length; i++){
+      answerArray[i] = '_';
+    }
+console.log(Object.values(answerArray));
+}
+ /*var display ='';
   for(var i=0; i < wordBank.length; i++){
     display = display + '_';
   }
-  console.log(letterInput.value);
-}
- //------------------------------------------------------
-   function guessOne(){
-     var guess = document.getElementById('letter');
-     var showThisMessage = '';
+  word.innerHTML = wordBank; */
 
-       if(wordBank.length !== 1){
-         showThisMessage = 'Please enter one letter.'
-       } else {
-           var i = 0;
-             for (i = 0; i < wordBank.length; i++){
-               if (word[i] === guess);
-    }
-  }
-}
+
+ //------------------------------------------------------
+
 //----------------------------------------------------
 //function usersGuess() {}
