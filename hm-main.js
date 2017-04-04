82ea068ae673b8b8a2ff3f4b91a7c65683adec56
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-04 01:02:44 -0400

    start game and input button active, but need to work together

--- hm-main.js
+++ hm-main.js
@@ -1,46 +1,28 @@
 
 let numOfMisses = 0;
 const maxMisses =7;
-let currentWord = null;
+//let currentWord = null;
 var wordToGuess = '';
 var wordBank = ['apple', 'oatmeal', 'rice', 'kale', ];
 var word = document.getElementById('blank-word');
-var letters = document.getElementById('start-button');
+//var letters = document.getElementById('start-button');
 var letterInput = document.getElementById('letter');
+/*var el = document.getElementById('submit');
+el.innerHTML = wordsBank;*/
+var currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
 
 // start button is working; needs to pick random number and display underscore for that word
 document.getElementById('new-game').onclick = function startGame() {
-
-          var placeholder = '',
-          abc = ['A','B','C','D','E','F','G','H','I','J','K','L',
-                'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-          numOfMisses = 0;
-          wordToGuess = getWord();
-          wordLength = wordToGuess.length;
-
-          for (var i = 0; i < wordLength; i++) {
-            placeholder += '_';
-            }
-          word.innerHTML = placeholder;
+    var answerArray = [];
+    var display ='';
+      for (var i = 0; i < currentWord.length; i++){
+        answerArray[i] = '_';
+        display = display + '_';
+      }
+      document.getElementById('blank-word').innerHTML = display;
+  //console.log(Object.values(answerArray));
 }
-      function getWord(word) {
-        return currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ];
-}//----------------------------------------------------
-document.getElementById('submit').onclick = function displayBlankWord(){
-  var answerArray = [];
-    for (var i = 0; i < wordBank.length; i++){
-      answerArray[i] = '_';
-    }
-console.log(Object.values(answerArray));
+document.getElementById('submit').onclick = function displayLetter(){
+  console.log('hello');
+  // needs to display transfer value from input to screen
 }
- /*var display ='';
-  for(var i=0; i < wordBank.length; i++){
-    display = display + '_';
-  }
-  word.innerHTML = wordBank; */
-
-
- //------------------------------------------------------
-
-//----------------------------------------------------
-//function usersGuess() {}
