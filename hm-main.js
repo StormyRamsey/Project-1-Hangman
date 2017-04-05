cef4fba5a8956f074d6fb1d42fe6ed1b60290c92
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-05 11:12:00 -0400

    attempt to activate input to show between letter

--- hm-main.js
+++ hm-main.js
@@ -1,13 +1,9 @@
 
 var numOfMisses = 0;
 const maxMisses =7;
-//let currentWord = null;
 var wordBank = ['apple', 'oatmeal', 'rice', 'kale'];
 var word = document.getElementById('text');
-//var letters = document.getElementById('start-button');
 var letterInput = document.getElementById('letter');
-/*var el = document.getElementById('submit');
-el.innerHTML = wordsBank;*/
 var currentWord = null;
 var currentLetters = null
 var lettersGuessed = []
@@ -29,6 +25,12 @@ function displayLetters() {
   })
   $('#text').text(lettersDisplayed.join(''))
 }
+//-Current Function in progess-
+$('#submit').on('click', function(displayLetters) {
+    $(lettersGuessed).push(displayLetters)
+    $('#letter').text(displayLetters)
+
+})
 
 
 
