8b9b03cd919754c144596092539e01f896be17c1
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-05-18 21:01:18 -0400

    added css

--- hm-main.js
+++ hm-main.js
@@ -1,19 +1,22 @@
 
 var numOfMisses = 0;
 var maxMisses = 6;
-var wordBank = ['apple', 'oatmeal', 'rice', 'kale'];
+var wordBank = ['apple', 'oatmeal', 'rice', 'kale', 'orange', 'banana', 'peach', 'pear', 'cucumber', 'melon', 'watermelon', 'spinach', 'onion', 'parsley', 'broccoli'];
 var word = document.getElementById('text');
 var letterInput = $('#letter');
 var currentWord = null;
 var currentLetters = null
 var lettersGuessed = []
 
+// This function starts the game by pulling a random word from the wordBank array and calling the displayLetters function.
 function startGame() {
   currentWord = wordBank[ Math.floor(Math.random() * wordBank.length) ]
   currentLetters = currentWord.split('')
   displayLetters()
 }
-
+// The displayLetters function maps the letter that was guessed
+// into the lettersGuessed array and if the letter is correct the letter will display.
+// If not the function will display an underscore.
 function displayLetters() {
   let lettersDisplayed = currentLetters.map((letter) => {
     if(lettersGuessed.includes(letter)) {
@@ -21,11 +24,10 @@ function displayLetters() {
     } else {
       return ' _ '
     }
-
   })
 
-  $('#text').text(lettersDisplayed.join(''))
-   return lettersDisplayed.join('')
+$('#text').text(lettersDisplayed.join(' '))
+   return lettersDisplayed.join(' ')
 }
 
 $('#submit').on('click', function(letter) {
@@ -38,15 +40,12 @@ $('#submit').on('click', function(letter) {
     lettersGuessed.push(input)
 
     let lettersDisplayed = displayLetters()
-    //if (lettersDisplayed.includes(!== ' _ '))
     if (currentWord.includes(input)) {
       $('#alert-box1').text('Good Guess!')
     } else {
-      //numMissed += 1
       $('#alert-box1').text('Try Again!')
+      }
     }
-  }
-
   })
 
 $('#new-game').on('click', startGame)
